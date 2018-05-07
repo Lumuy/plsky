383f3e4dc68172ad75139e0ebd55db160904586f
lumuy <dxwang68@gmail.com>
2018-05-08 04:23:03 +0800

    clear unused files

--- app/assets/javascripts/header.js
+++ app/assets/javascripts/header.js
@@ -1,6 +1,12 @@
-$(document).on('turbolinks:reload', function(){
+$(document).on('turbolinks:load', function(){
   // header mobile menu click
   $('header .sh-mobile-ctrl').on('click', function(){
-    $('header .sh-menu').toggleClass('showing');
+    var tg = $('header .sh-menu')
+    if(tg.css('display') == 'none') {
+      tg.css('display', 'block');
+    } else {
+      tg.css('display', 'none');
+    }
+    // $('header .sh-menu ul').toggleClass('showing');
   });
 });
