8294eec43433b38477793290ef9cc91393f5f76c
lumuy <dxwang68@gmail.com>
2018-05-16 16:13:20 +0800

    fix

--- app/uploaders/photo_uploader.rb
+++ app/uploaders/photo_uploader.rb
@@ -13,7 +13,7 @@ class PhotoUploader < CarrierWave::Uploader::Base
   #   "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   # end
   def store_dir
-    'public/uploads/photos/'
+    'uploads/photos/'
   end
 
   # Provide a default URL as a default if there hasn't been a file uploaded:
